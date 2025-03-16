0ac4347f42ae64f89078df288f5b628d1c033019
Aaron Ritter <git@ritter.world>
2025-03-16 23:44:21 +0100

    chore(main): release 1.9.7 (#76)

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -72,7 +72,7 @@ publishing {
                 groupId = "llc.sonderformat"
                 artifactId = "fusionauth-android-sdk"
                 // x-release-please-start-version
-                version = "1.9.7-rc"
+                version = "1.9.7"
                 // x-release-please-end
 
                 // And here are some more properties that go into the pom file.
