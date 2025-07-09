ba1ead8709f52e89f813bed807bf14ce310b9321
Dom <133953108+thedoom13@users.noreply.github.com>
2025-07-09 11:18:31 +0700

    ci(release): change prerelease-type to takai

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -72,7 +72,7 @@ publishing {
                 groupId = "llc.sonderformat"
                 artifactId = "fusionauth-android-sdk"
                 // x-release-please-start-version
-                version = "1.11.0-rc"
+                version = "1.11.0-takai"
                 // x-release-please-end
 
                 // And here are some more properties that go into the pom file.
